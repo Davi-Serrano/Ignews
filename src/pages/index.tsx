b00e3cccf31c13576a6d135d15e04a6ab4e5dcee
Davi Serrano <daviscardoso2901@gmail.com>
2021-11-15 18:55:56 -0300

    Component SubscribeButton Created

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import Head from "next/head"
+import { SubscribeButton } from "../components/SubscribeButton"
 
 import styles from "./home.module.scss"
 export default function Home() {
@@ -17,10 +18,12 @@ export default function Home() {
             Get acess to all the publications <br />
             <span>For 9.98 month</span>
           </p>
+
+          <SubscribeButton />
       </section>  
 
       <img src="/images/Avatar.svg" alt="Avatar" />
-    </main>>
+    </main>
     </>
   )
 }
