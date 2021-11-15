77abd27020eb73dd4aee4c6cc60a890b53f6ee51
Davi Serrano <daviscardoso2901@gmail.com>
2021-11-14 22:26:51 -0300

    Dinamic Title

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,18 @@
+import Head from "next/head"
+
 import styles from "../styles/home.module.scss"
 
 export default function Home() {
   return (
+    <>
+     <Head>
+       <title>  Inicio | Ig.News </title>
+     </Head>
+     
     <h1 className={styles.titles}>
-      Hellow <span>Word</span> </h1>
+     
+      Hellow <span>Word</span> 
+    </h1>
+    </>
   )
 }
