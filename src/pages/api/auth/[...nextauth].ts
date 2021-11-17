336f57beae760f1e7f12cbb926681a5db9a0d907
Davi Serrano <daviscardoso2901@gmail.com>
2021-11-17 16:46:08 -0300

    Save user in FaunaDB

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,13 +1,39 @@
+import  { query as q } from "faunadb"
+
 import NextAuth from "next-auth"
-import GithubProvider from "next-auth/providers/github"
+import Providers from "next-auth/providers"
+
+import { fauna } from "../../../services/fauna"
 
 export default NextAuth({
   // Configure one or more authentication providers
   providers: [
-    GithubProvider({
+    Providers.GitHub({
       clientId: process.env.GITHUB_CLIENT_ID,
       clientSecret: process.env.GITHUB_CLIENT_SECRET,
       scope: 'read:user'
     }),
   ],
+  callbacks: {
+    async signIn(user, account, profile ) {
+      
+      const { email } = user
+
+      try{
+        await fauna.query(
+          q.Create(
+            q.Collection('users'),
+            { data: { email }}
+            )
+          )
+          return true
+
+      } catch{
+          return false
+      }
+        
+     
+      
+    },
+  }
 })
