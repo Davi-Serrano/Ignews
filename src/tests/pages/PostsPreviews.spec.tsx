69e19482478362525df679ee8757bc699a037656
Davi Serrano <daviscardoso2901@gmail.com>
2022-03-12 19:05:27 -0300

    Teste post preview finalizado

--- src/tests/pages/PostsPreviews.spec.tsx
+++ src/tests/pages/PostsPreviews.spec.tsx
@@ -3,6 +3,7 @@ import Post, { getStaticProps } from "../../pages/posts/previews/[slug]"
 import { mocked } from "ts-jest/utils"
 import { getPrismicClient } from "../../services/prismic"
 import { useSession } from "next-auth/client"
+import { useRouter } from "next/router"
 
 
 const post = {
@@ -14,7 +15,7 @@ const post = {
 
 jest.mock("../../services/prismic")
 jest.mock("next-auth/client")
-
+jest.mock("next/router")
 
 describe("Post preview Page", ()=>{
     it("Renders correctly", ()=>{
@@ -29,64 +30,59 @@ describe("Post preview Page", ()=>{
         expect(screen.getByText("Wanna continue reading ?")).toBeInTheDocument
     })
 
-    // it("redirects user if no subscription is found", async ()=>{
-    //     const getSessionMocked = mocked(getSession)
+    it("redirects user to full post when user is subscribed", async ()=>{
+        const useSessionMocked = mocked(useSession)
+        const useRouterMocked = mocked(useRouter)
+        const pushMock = jest.fn()
 
-    //     getSessionMocked.mockResolvedValueOnce(null) 
+        useSessionMocked.mockReturnValueOnce([
+            {activeSubscription: "fake-active-subscription"},
+            false
+        ] as any)
 
-    //     const response = await getServerSideProps({
-    //         params:{
-    //             slug: "my-new-post",
-    //         },
-    //     } as any)
+        useRouterMocked.mockReturnValueOnce({
+            push: pushMock,
+        } as any)
 
-    //     expect(response).toEqual(
-    //         expect.objectContaining({
-    //            redirect: expect.objectContaining({
-    //                 destination: "/"  
-    //            })
-    //         })
-    //     )
-    // })
+        render(<Post post={post}/>)
 
-    // it("loading data", async ()=>{
-    //     const getSessionMocked = mocked(getSession)
-    //     const getPrismicClientMOcked = mocked(getPrismicClient)
+        expect(pushMock).toHaveBeenCalledWith("/posts/my-new-post")
 
-    //     getPrismicClientMOcked.mockReturnValueOnce({
-    //         getByUID: jest.fn().mockResolvedValueOnce({
-    //             data: {
-    //                 title: "my-new-post",
-    //                 content: [
-    //                     {type: "paragraph", text: "Post excerpt"}
-    //                 ],
-    //             },
-    //             last_publication_date: "04-01-2021"
-    //         })
         
-    //     } as any)
+    })
 
-    //     getSessionMocked.mockResolvedValueOnce({
-    //         activeSubscription: "fake-active-subscription",
-    //     } as any)
+    it("loading data", async ()=>{
+        const getPrismicClientMOcked = mocked(getPrismicClient)
+
+        getPrismicClientMOcked.mockReturnValueOnce({
+            getByUID: jest.fn().mockResolvedValueOnce({
+                data: {
+                    title: "my-new-post",
+                    content: [
+                        {type: "paragraph", text: "Post excerpt"}
+                    ],
+                },
+                last_publication_date: "04-01-2021"
+            })
+        
+        } as any)
 
 
-    //     const response = await getServerSideProps({
-    //         params:{ slug: "my-new-post" },
-    //     } as any)
 
-    //     expect(response).toEqual(
-    //         expect.objectContaining({
-    //            props: {
-    //                post: {
-    //                    slug: "my-new-post",
-    //                    title: undefined,
-    //                    content: '<p>Post excerpt</p>',
-    //                    updatedAt: "01 de abril de 2021" 
-    //                }
-    //            }
-    //         })
-    //     )
-    // })
+        const response = await getStaticProps({params: {slug: "my-new-post"}})
+
+        expect(response).toEqual(
+            expect.objectContaining({
+               props: {
+                   post: {
+                       slug: "my-new-post",
+                       title: undefined,
+                       content: '<p>Post excerpt</p>',
+                       updatedAt: "01 de abril de 2021" 
+                   }
+               }
+            })
+        )
+    })
     
 })
