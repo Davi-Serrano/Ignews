77fe88e9cf67c6ed5ed09ea9c8ad334211a87559
Davi Serrano <daviscardoso2901@gmail.com>
2022-03-12 12:36:05 -0300

    Primiero testa pagina de um post

--- src/tests/pages/Post.spec.tsx
+++ src/tests/pages/Post.spec.tsx
@@ -1,60 +1,61 @@
 import {render, screen } from "@testing-library/react"
-import Posts, { getStaticProps } from "../../pages/posts"
+import Post, { getServerSideProps } from "../../pages/posts/[slug]"
 import { mocked } from "ts-jest/utils"
 import { getPrismicClient } from "../../services/prismic"
 
 
-const posts = [{
+const post = {
     slug: "my-new-post",
     title: "My New Post",
-    excerpt: "Post excerpt",
+    content: "<p>Post excerpt</p>",
     updatedAt: "March 10"
-}]
+}
 
 jest.mock("../../services/prismic")
 
 
 describe("Post Page", ()=>{
     it("Renders correctly", ()=>{
-        render(<Posts posts={posts} />)
+        render(<Post post={post} />)
 
         expect(screen.getByText("My New Post")).toBeInTheDocument
+        expect(screen.getByText("Post excerpt")).toBeInTheDocument
     })
 
-    it("loads initial data", async ()=>{
-        const getPrismicClientMocked = mocked(getPrismicClient)
+    // it("loads initial data", async ()=>{
+    //     const getPrismicClientMocked = mocked(getPrismicClient)
 
-        getPrismicClientMocked.mockReturnValueOnce({
-            query: jest.fn().mockResolvedValueOnce({
-                results: [
-                    {
-                        uid: "my-new-post",
-                        data: {
-                            title: "",
-                            content: [
-                                {type: "paragraph", text: "Post excerpt"}
-                            ],
-                        },
-                        last_publication_date: "04-01-2021"
-                    }
-                ]
-            })
-        } as any)
+    //     getPrismicClientMocked.mockReturnValueOnce({
+    //         query: jest.fn().mockResolvedValueOnce({
+    //             results: [
+    //                 {
+    //                     uid: "my-new-post",
+    //                     data: {
+    //                         title: "",
+    //                         content: [
+    //                             {type: "paragraph", text: "Post excerpt"}
+    //                         ],
+    //                     },
+    //                     last_publication_date: "04-01-2021"
+    //                 }
+    //             ]
+    //         })
+    //     } as any)
 
         
-        const response = await getStaticProps({})
+    //     const response = await getStaticProps({})
 
-        expect(response).toEqual(
-            expect.objectContaining({
-                props: {
-                    posts: [{
-                        slug: "my-new-post",
-                        title: "my-new-post",
-                        excerpt: "Post excerpt",
-                        updatedAt: "01 de abril de 2021"
-                    }]
-                }
-            })
-        )
-    })
+    //     expect(response).toEqual(
+    //         expect.objectContaining({
+    //             props: {
+    //                 posts: [{
+    //                     slug: "my-new-post",
+    //                     title: "my-new-post",
+    //                     excerpt: "Post excerpt",
+    //                     updatedAt: "01 de abril de 2021"
+    //                 }]
+    //             }
+    //         })
+    //     )
+    // })
 })
