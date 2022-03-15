3fb671d71e45fd0c35022307c6c8052327aa1f1e
Davi Serrano <daviscardoso2901@gmail.com>
2022-03-14 22:50:04 -0300

    Tentando fazer deploy

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -52,7 +52,7 @@ export default function Posts({posts}: PostsProps){
 export const getStaticProps: GetStaticProps = async ()=>{
     const prismic = getPrismicClient()
 
-    const response = await prismic.query([
+    const response = await prismic.query<any>([
         Prismic.predicates.at('document.type', 'publication')
     ],{
         fetch: ['publication', 'publication.content'],
